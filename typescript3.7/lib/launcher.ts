dd614d14852c3b60a89b7089ffda1750eb244cee
Michele Sciabarra <msciabarra@apache.org>
2019-12-27 14:03:27 +0100

    fixed issues in the launcher

--- typescript3.7/lib/launcher.ts
+++ typescript3.7/lib/launcher.ts
@@ -1,23 +1,31 @@
-let main = require("./main__")
-let fs = require("fs")
+const main = require("./main__")
+const readline = require('readline');
+const fs = require("fs")
 
-process.stdin.setEncoding('utf8');
-let out = fs.createWriteStream(null, { fd: 3 });
-process.stdin.on('readable', function() {
-  var line = process.stdin.read();
-  if (line !== null) {
+async function actionLoop() {
+  const out = fs.createWriteStream(null, 
+    { fd: 3, encoding: "utf8" })
+  process.stdin.setEncoding('utf8');
+  const rl = readline.createInterface({
+    input: process.stdin
+  });
+  for await (const line of rl) {
     try {
-        let args = JSON.parse(line)
-        let value = args.value || {}
-        for (let key in args) {
-            if(key !== "value")
-              process.env["_OW_"+key.toUpperCase()] = args[key]
-        }
-        let result = main(value)
-        out.write(JSON.stringify(result)+"\n");
+      let args = JSON.parse(line)
+      let value = args.value || {}
+      for (let key in args) {
+          if(key !== "value") {
+            let envar = "_OW_"+key.toUpperCase()
+            process.env[envar] = args[key]
+          }
+      }
+      let result = main.main(value)
+      out.write(JSON.stringify(result)+"\n");
     } catch(err) {
-        let error = {"error": err}
-        out.write(JSON.stringify(error)+"\n");
+      let message = err.message || err.toString()
+      let error = {"error": message}
+      out.write(JSON.stringify(error)+"\n");
     }
   }
-})
+}
+actionLoop()
